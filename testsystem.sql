c9b2ae8f6ec0cb473f47d5a243633653aacbb13d
ansvir <46724063+ansvir@users.noreply.github.com>
2020-12-17 21:43:43 +0300

    test system database

--- testsystem.sql
+++ testsystem.sql
@@ -16,6 +16,159 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `choice`
+--
+
+DROP TABLE IF EXISTS `choice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `choice` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `choice`
+--
+
+LOCK TABLES `choice` WRITE;
+/*!40000 ALTER TABLE `choice` DISABLE KEYS */;
+INSERT INTO `choice` VALUES (1,'8'),(2,'2'),(3,'4'),(4,'5'),(5,'2'),(6,'4'),(7,'5'),(8,'7'),(9,'8'),(10,'2'),(11,'4'),(12,'10'),(13,'4'),(14,'2'),(15,'7'),(16,'8');
+/*!40000 ALTER TABLE `choice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `code` varchar(50) DEFAULT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+INSERT INTO `permission` VALUES (1,'Create a user','cu','create user description'),(2,'Create a test','crt','create test description'),(3,'Take a test','tat','take a test description');
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions_roles`
+--
+
+DROP TABLE IF EXISTS `permissions_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permissions_roles` (
+  `permission_id` int DEFAULT NULL,
+  `role_id` int DEFAULT NULL,
+  KEY `permissions_roles_permission_id_fk` (`permission_id`),
+  KEY `permissions_roles_role_id_fk` (`role_id`),
+  CONSTRAINT `permissions_roles_permission_id_fk` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`id`),
+  CONSTRAINT `permissions_roles_role_id_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions_roles`
+--
+
+LOCK TABLES `permissions_roles` WRITE;
+/*!40000 ALTER TABLE `permissions_roles` DISABLE KEYS */;
+INSERT INTO `permissions_roles` VALUES (1,1),(2,1),(3,1);
+/*!40000 ALTER TABLE `permissions_roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question`
+--
+
+DROP TABLE IF EXISTS `question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question`
+--
+
+LOCK TABLES `question` WRITE;
+/*!40000 ALTER TABLE `question` DISABLE KEYS */;
+INSERT INTO `question` VALUES (1,'4+2?'),(2,'4-2?'),(3,'4*2?'),(4,'4/2?');
+/*!40000 ALTER TABLE `question` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `question_answer`
+--
+
+DROP TABLE IF EXISTS `question_answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `question_answer` (
+  `question_id` int DEFAULT NULL,
+  `answer_id` int DEFAULT NULL,
+  UNIQUE KEY `question_answer_answer_id_uindex` (`answer_id`),
+  UNIQUE KEY `question_answer_question_id_uindex` (`question_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `question_answer`
+--
+
+LOCK TABLES `question_answer` WRITE;
+/*!40000 ALTER TABLE `question_answer` DISABLE KEYS */;
+INSERT INTO `question_answer` VALUES (1,1),(2,5),(3,9),(4,14);
+/*!40000 ALTER TABLE `question_answer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `questions_choices`
+--
+
+DROP TABLE IF EXISTS `questions_choices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `questions_choices` (
+  `question_id` int DEFAULT NULL,
+  `choice_id` int DEFAULT NULL,
+  KEY `questions_choices_choice_id_fk` (`choice_id`),
+  KEY `questions_choices_question_id_fk` (`question_id`),
+  CONSTRAINT `questions_choices_choice_id_fk` FOREIGN KEY (`choice_id`) REFERENCES `choice` (`id`),
+  CONSTRAINT `questions_choices_question_id_fk` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `questions_choices`
+--
+
+LOCK TABLES `questions_choices` WRITE;
+/*!40000 ALTER TABLE `questions_choices` DISABLE KEYS */;
+INSERT INTO `questions_choices` VALUES (1,1),(1,2),(1,3),(1,4),(2,5),(2,6),(2,7),(2,8),(3,9),(3,10),(3,11),(3,12),(4,13),(4,14),(4,15),(4,16);
+/*!40000 ALTER TABLE `questions_choices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `role`
 --
 
@@ -38,6 +191,33 @@ LOCK TABLES `role` WRITE;
 /*!40000 ALTER TABLE `role` DISABLE KEYS */;
 INSERT INTO `role` VALUES (1,'administrator','administrator description'),(2,'tutor','tutor description'),(3,'student','student description');
 /*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles_users`
+--
+
+DROP TABLE IF EXISTS `roles_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles_users` (
+  `role_id` int DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  KEY `roles_users_role_id_fk` (`role_id`),
+  KEY `roles_users_user_id_fk` (`user_id`),
+  CONSTRAINT `roles_users_role_id_fk` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`),
+  CONSTRAINT `roles_users_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles_users`
+--
+
+LOCK TABLES `roles_users` WRITE;
+/*!40000 ALTER TABLE `roles_users` DISABLE KEYS */;
+INSERT INTO `roles_users` VALUES (1,1);
+/*!40000 ALTER TABLE `roles_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -66,6 +246,33 @@ INSERT INTO `subject` VALUES (1,'mathematics','mathematics description'),(2,'eng
 UNLOCK TABLES;
 
 --
+-- Table structure for table `subjects_tests`
+--
+
+DROP TABLE IF EXISTS `subjects_tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `subjects_tests` (
+  `subject_id` int DEFAULT NULL,
+  `test_id` int DEFAULT NULL,
+  KEY `subjects_tests_subject_id_fk` (`subject_id`),
+  KEY `subjects_tests_test_id_fk` (`test_id`),
+  CONSTRAINT `subjects_tests_subject_id_fk` FOREIGN KEY (`subject_id`) REFERENCES `subject` (`id`),
+  CONSTRAINT `subjects_tests_test_id_fk` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subjects_tests`
+--
+
+LOCK TABLES `subjects_tests` WRITE;
+/*!40000 ALTER TABLE `subjects_tests` DISABLE KEYS */;
+INSERT INTO `subjects_tests` VALUES (1,1);
+/*!40000 ALTER TABLE `subjects_tests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `test`
 --
 
@@ -78,7 +285,7 @@ CREATE TABLE `test` (
   `description` varchar(50) DEFAULT NULL,
   `time` time DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,7 +294,35 @@ CREATE TABLE `test` (
 
 LOCK TABLES `test` WRITE;
 /*!40000 ALTER TABLE `test` DISABLE KEYS */;
+INSERT INTO `test` VALUES (1,'mathematics test','mathematics test descrption',NULL);
 /*!40000 ALTER TABLE `test` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tests_questions`
+--
+
+DROP TABLE IF EXISTS `tests_questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tests_questions` (
+  `test_id` int DEFAULT NULL,
+  `question_id` int DEFAULT NULL,
+  KEY `tests_questions_question_id_fk` (`question_id`),
+  KEY `tests_questions_test_id_fk` (`test_id`),
+  CONSTRAINT `tests_questions_question_id_fk` FOREIGN KEY (`question_id`) REFERENCES `question` (`id`),
+  CONSTRAINT `tests_questions_test_id_fk` FOREIGN KEY (`test_id`) REFERENCES `test` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tests_questions`
+--
+
+LOCK TABLES `tests_questions` WRITE;
+/*!40000 ALTER TABLE `tests_questions` DISABLE KEYS */;
+INSERT INTO `tests_questions` VALUES (1,1),(1,2),(1,3),(1,4);
+/*!40000 ALTER TABLE `tests_questions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -124,4 +359,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-17 20:14:33
+-- Dump completed on 2020-12-17 21:42:42
